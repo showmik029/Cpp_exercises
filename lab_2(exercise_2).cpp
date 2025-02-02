6e2b488c855c332bbb82ee141bfb2747e17a4afb
Akib <showmik029@gmail.com>
2025-02-02 03:35:06 +0200

    Completed Exercise 1

---
+++ lab_2(exercise_2).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void replaceCharacter(string &str, char ch) {
+    for (char &c : str) {
+        if (c == ch) {
+            c = '_';
+        }
+    }
+}
+
+int main() {
+    string inputString;
+    char replaceChar;
+    string stop;
+
+    while (true) {
+        cout << "Enter a string: ";
+        getline(cin, inputString);
+        cout << "Enter character to replace or \"stop\": ";
+        cin >> stop;
+        if (stop == "stop") {
+            break;
+        }
+        replaceChar = stop[0];
+        replaceCharacter(inputString, replaceChar);
+        cout << "Result: " << inputString << endl;
+        cin.ignore();
+    }
+
+    return 0;
+}
+
