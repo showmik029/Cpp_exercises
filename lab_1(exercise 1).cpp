6cb7530dc35dbbf5c773961efbb0986a02ea0c0d
Akib <showmik029@gmail.com>
2025-02-02 02:56:57 +0200

    Completed Exercise 1

--- lab_1(exercise 1).cpp
+++ lab_1(exercise 1).cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <vector>
 
 int main()
-{
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}efsef
-// bro wtf//
+{  int num;
+    std::cout << "How many numbers do you want to enter?: ";
+    std::cin >> num;
+    std::vector<int>numbers;
+    for (int i = 1; i <= num; i++)
+    {   int temp;
+        std::cout <<"Enter nr["<<i<<"]: ";
+        std::cin >> temp;
+        numbers.push_back(temp);
+    }
+    std::cout <<"The numbers you entered are: ";
+    int count=0;
+    for (int num : numbers)
+    {
+        std::cout << num;
+        count++;
+        if (count !=numbers.size())
+        {
+            std::cout << ",";
+        }
+    }
+
+
+}
